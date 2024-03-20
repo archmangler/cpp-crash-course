3d76da8d0147f11ec6ccc360a1301b1faa261e96
Traiano Welcome <traiano@gmail.com>
2024-03-20 22:06:31 +0800

    header files and macros

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include "main.h"
+#include <iostream>
 
 void test();
 
 int main(int argc, char** argv){
 
+    test();
+    
     return 0;
-
 }
-
 
 void test()
 {
-
+ std::cout << "Hello Universe!\n" ;
 }
