6ef16d17b6a0a9c2b12881a4d4f6874092984035
Traiano Welcome <traiano@gmail.com>
2024-03-21 22:13:21 +0800

    cpp file io: input

--- main.cpp
+++ main.cpp
@@ -1,33 +1,27 @@
-#include <vector>
+#include <fstream>
 #include <iostream>
+#include <string>
 
 int main(int argc, char**  argv)
 {
-    std::vector<int> vec;
-    vec.push_back(55);
-    vec.push_back(53);
-    vec.push_back(53);
-    vec.push_back(56);
-    vec.push_back(45);
-    vec.push_back(65);
-    vec.push_back(75);
-    vec.push_back(15);
-    vec.push_back(45);
 
-for (int i=0;i<vec.size(); i++) 
+std::ifstream file("file.dat");
+
+if(file.is_open())
 {
-    std::cout << "vec[i] -> " << vec[i] << std::endl;
+    std::cout << "The file is open." << std::endl;
+}
+else
+{
+    std::cout << "problem opening the file: "<< std::endl;
+    return 1;
 }
 
-vec.pop_back();
+std::string msg;
 
-
-std::cout << "pop back ?" <<"\n";
-
-for (int i=0;i<vec.size(); i++) 
+while(getline(file,msg))
 {
-    std::cout << "vec[i] -> " << vec[i] << std::endl;
+    std::cout << "file line -> " << msg << std::endl;
 }
-
     return 0;
 }
