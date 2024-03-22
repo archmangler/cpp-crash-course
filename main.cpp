b8ac45980c0ccb2199d931ddf2d30fcdc67dfa9f
Traiano Welcome <traiano@gmail.com>
2024-03-23 05:38:38 +0800

    structs in cpp

--- main.cpp
+++ main.cpp
@@ -2,37 +2,47 @@
 #include <iostream>
 #include <string>
 
+struct coordinate
+{
+    float longitude;
+    float latitude;
+    float altitude;
+};
+
+struct system
+{
+    struct coordinate current_coord;
+    struct coordinate last_coord;
+    struct coordinate planned_coord;
+};
+
 int main(int argc, char**  argv)
 {
+    struct coordinate coord_001;
+    struct coordinate coord_002;
+    struct coordinate coord_003;
 
-std::streampos begin, end; //preparing to determine size of file
-std::ifstream file("file.dat", std::ios::binary);
+    coord_001.latitude = 0.1;
+    coord_001.longitude = 0.2;
+    coord_001.altitude = 0.3;
 
-if(file.is_open())
-{
-    std::cout << "The file is open." << std::endl;
-}
-else
-{
-    std::cout << "problem opening the file: "<< std::endl;
-    return 1;
-}
+    coord_001.latitude = 0.5;
+    coord_001.longitude = 0.6;
+    coord_001.altitude = 0.8;
+
+    coord_001.latitude = 1;
+    coord_001.longitude = 2;
+    coord_001.altitude = 3;
 
 
-begin = file.tellg();
-file.seekg(0, std::ios::end);
-end = file.tellg();
+    struct system thing;
 
-std::cout << "File size: " << (end - begin) << std::endl;
-std::string msg;
-
-/* When reading out file contents ...
-while(getline(file,msg))
-{
-    std::cout << "file line -> " << msg << std::endl;
-}
-*/
-
+    thing.current_coord = coord_001;
+    thing.last_coord = coord_002;
+    thing.planned_coord = coord_003;
+    
+    std::cout << "coord_001: " << coord_001.longitude << " " << coord_001.latitude << " " << coord_001.altitude << std::endl;
+    std::cout << "system coordinates (latitude): " << thing.current_coord.latitude << std::endl;
 
     return 0;
 }
