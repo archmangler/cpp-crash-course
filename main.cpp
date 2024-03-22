65192c7b2af48fd235d179bc9b51d2ac25194f5e
Traiano Welcome <traiano@gmail.com>
2024-03-23 06:09:59 +0800

    basic pointers and references

--- main.cpp
+++ main.cpp
@@ -2,47 +2,17 @@
 #include <iostream>
 #include <string>
 
-struct coordinate
-{
-    float longitude;
-    float latitude;
-    float altitude;
-};
-
-struct system
-{
-    struct coordinate current_coord;
-    struct coordinate last_coord;
-    struct coordinate planned_coord;
-};
-
 int main(int argc, char**  argv)
 {
-    struct coordinate coord_001;
-    struct coordinate coord_002;
-    struct coordinate coord_003;
-
-    coord_001.latitude = 0.1;
-    coord_001.longitude = 0.2;
-    coord_001.altitude = 0.3;
-
-    coord_001.latitude = 0.5;
-    coord_001.longitude = 0.6;
-    coord_001.altitude = 0.8;
-
-    coord_001.latitude = 1;
-    coord_001.longitude = 2;
-    coord_001.altitude = 3;
-
-
-    struct system thing;
-
-    thing.current_coord = coord_001;
-    thing.last_coord = coord_002;
-    thing.planned_coord = coord_003;
     
-    std::cout << "coord_001: " << coord_001.longitude << " " << coord_001.latitude << " " << coord_001.altitude << std::endl;
-    std::cout << "system coordinates (latitude): " << thing.current_coord.latitude << std::endl;
+    int a = 50; //set the value of a variable to 50
+    
+    std::cout << "current value of a: " << a << std::endl;
+
+    int* ptr = &a; //create a pointer called ptr which points to the location of the variable a in mem.
+    *ptr = 25; //set the value inside the location ptr is pointing to to 25 (change the value inside variable a from 50 to 25)
+    
+    std::cout << "new value of a: " << a << std::endl;
 
     return 0;
 }
