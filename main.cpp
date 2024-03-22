41fd6ca212842a3cb381efab3553428907bafa5c
Traiano Welcome <traiano@gmail.com>
2024-03-23 06:25:25 +0800

    intermediate pointers and references

--- main.cpp
+++ main.cpp
@@ -3,18 +3,21 @@
 #include <string>
 
 int main(int argc, char**  argv)
-{
-    
+{    
     int a = 50; //set the value of a variable to 50
     
     std::cout << "current value of a: " << a << std::endl;
 
     int* ptr = &a; //create a pointer called ptr which points to the location of the variable a in mem.
+    int** ptrptr = &ptr; //declare a pointer to the pointer
+
     *ptr = 25; //set the value inside the location ptr is pointing to to 25 (change the value inside variable a from 50 to 25)
     
     std::cout << "new value of a: " << a << std::endl;
 
     std::cout << "pointer value: " << ptr << " pointer address contents: " << *ptr << " address of variable 'a': " << &a << std::endl;
+
+    std::cout << "pointer-to-pointer value: " << ptrptr << " pointer-to-pointer address contents: " << *ptrptr << " contents of that address: " <<  **ptrptr << std::endl; 
 
     return 0;
 }
